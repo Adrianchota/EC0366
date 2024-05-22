6c89cbe005da91d4d3fe76afd15d9964710c48ed
Adrian Sauceda <adrian.sauceda@beesoftware.com>
2024-05-21 22:58:57 -0600

    Few Changes El

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
+    <LinearGradient
+        className="flex-1 items-center justify-center"
+        colors={[
+          '#23232C'
+        ]}
+        style={styles.background}
+      >
+        <Text>Mi vida Elizabeth</Text>
+      </LinearGradient>
   );
 }
 
@@ -28,3 +35,15 @@ function App() {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'orange',
+  },
+  background: {
+
+  }
+});
